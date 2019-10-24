87393dafc56c4c529ef402a7bb157472c0f88aea
Le8wa-dev <savinchev@list.ru>
2019-10-24 16:22:23 +0300

    added section surf

--- app/js/main.js
+++ app/js/main.js
@@ -5,14 +5,30 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows-left" src="images/arrows-left.svg" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows-right" src="images/arrows-right.svg" alt="">',
-        asNavFor: '.slider-dots',
+        asNavFor: '.slider-dotshead',
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
 
+    });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="images/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="images/arrows-right.svg" alt="">',
+        asNavFor: '.slider-map',
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true,
     });
   
 });
